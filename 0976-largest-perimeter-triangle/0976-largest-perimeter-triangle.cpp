7ae09722a4003521940372c9ab332e738461cd87
Sakshi Agrahari <agraharisakku@gmail.com>
2025-09-28 14:01:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        int  maxp=0,p;
+        sort(nums.begin(),nums.end());
+        // for(int i =0;i<nums.size();i++){
+        //     for(int j=i+1;j<nums.size();j++){
+        //         for(int k=nums.size()-1;k>j;k--){
+        //             if(nums[i]+nums[j]>nums[k]&&nums[j]+nums[k]>nums[i]&&nums[k]+nums[i]>nums[j]){
+        //                 p=nums[i]+nums[j]+nums[k];
+        //                 maxp=max(p,maxp);
+        //             }
+        //         }
+        //     }
+        // }
+        // return maxp;
+        for(int k =nums.size()-1;k>=2;k--){
+            int i=0,j=k-1;
+            while(i<j){
+                if(nums[i]+nums[j]>nums[k]){
+                    p=nums[i]+nums[j]+nums[k];
+                    maxp=max(maxp,p);
+                    j--;
+                }
+                else{
+                    i++;
+                }
+            }
+        }
+        return maxp;
+    }
+};

918f97d74bda87ea5683d84dc04edc030ce616e4
Sakshi Agrahari <agraharisakku@gmail.com>
2025-11-05 21:22:12 +0530

    Time: 12 ms (41.68%), Space: 32.1 MB (17.61%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -4,13 +4,15 @@ public:
         sort(nums.begin(),nums.end());
         int n=nums.size();
         int sum=0;
-        int a;
-        for(int i =0;i<n-1;i+=2){
-            for(int j=i+1;j<n;j++){
-                a=min(nums[i],nums[j]);
-                sum+=a;
-                break;
-            }
+        //int a;
+        for(int i =0;i<n;i+=2){
+            sum+=nums[i];
+          
+            //  for(int j=i+1;j<n;j++){
+            //     a=min(nums[i],nums[j]);
+            //     sum+=a;
+            //     break;
+            //  } THIS WILL ALSO WORK 
 
         }
         return sum;

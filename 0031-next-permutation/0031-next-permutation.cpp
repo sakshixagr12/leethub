4488e8c141873ef91a43e3d3ea808c6afdc78644
Sakshi Agrahari <agraharisakku@gmail.com>
2026-02-09 17:58:42 +0530

    Time: 0 ms (100%), Space: 15.6 MB (66.98%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        if(next_permutation(nums.begin(),nums.end())==false){
-        sort(nums.begin(),nums.end());
+        int idx=-1;
+        int n = nums.size();
+        //finding the break point
+        for(int i =n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx=i;
+                break;
+            }
         }
-        
-        for(auto i : nums){
-            cout<<i<<" ";
+        // if array doesn't have next permutation , the first one will be the next permutation 
+        if(idx==-1){
+            reverse(nums.begin(),nums.end());
+            return;
         }
-        
+        //swapping with just greater element with nums[idx]
+        for(int i=n-1;i>=idx;i--){
+            if(nums[i]>nums[idx]){
+                swap(nums[i],nums[idx]);
+                break;
+            }
+        }
+        //arranging remaining in sorted order
+        reverse(nums.begin()+idx+1,nums.end());
     }
 };

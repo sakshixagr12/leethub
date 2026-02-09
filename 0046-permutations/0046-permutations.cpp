67022f95b031e6fb187903b07df3ec07fa876b33
Sakshi Agrahari <agraharisakku@gmail.com>
2026-02-09 18:18:24 +0530

    Time: 0 ms (100%), Space: 10.5 MB (87.69%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,3 +1,26 @@
+bool nextpermutations(vector<int>&nums){
+    int idx=-1;
+    int n= nums.size();
+    for(int i = n-2;i>=0;i--){
+        if(nums[i]<nums[i+1]){
+            idx=i;
+            break;
+        }
+    }
+    if(idx==-1){
+        reverse(nums.begin(),nums.end());
+        return false;
+    }
+    for(int i = n-1;i>=idx;i--){
+        if(nums[i]>nums[idx]){
+            swap(nums[i],nums[idx]);
+            break;
+        }
+    }
+    reverse(nums.begin()+idx+1,nums.end());
+    return true;
+
+}
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
@@ -6,7 +29,7 @@ public:
         do{
             ans.push_back(nums);
         }
-        while(next_permutation(nums.begin(),nums.end()));
+        while(nextpermutations(nums));
     
     return ans;
 }};

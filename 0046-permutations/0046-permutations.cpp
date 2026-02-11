bcd2d204581e13277f5d597a0cd3c06ed515fa6b
Sakshi Agrahari <agraharisakku@gmail.com>
2026-02-12 00:02:30 +0530

    Time: 0 ms (100%), Space: 10.6 MB (70.69%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,26 +1,26 @@
-bool nextpermutations(vector<int>&nums){
+bool nextp(vector<int>&arr){
     int idx=-1;
-    int n= nums.size();
-    for(int i = n-2;i>=0;i--){
-        if(nums[i]<nums[i+1]){
+    int n = arr.size();
+    for(int i =n-2;i>=0;i--){
+        if(arr[i]<arr[i+1]){
             idx=i;
             break;
         }
     }
     if(idx==-1){
-        reverse(nums.begin(),nums.end());
+        reverse(arr.begin(),arr.end());
         return false;
     }
-    for(int i = n-1;i>=idx;i--){
-        if(nums[i]>nums[idx]){
-            swap(nums[i],nums[idx]);
+    for(int k=n-1;k>=idx;k--){
+        if(arr[k]>arr[idx]){
+            swap(arr[k],arr[idx]);
             break;
         }
     }
-    reverse(nums.begin()+idx+1,nums.end());
+    reverse(arr.begin()+idx+1,arr.end());
     return true;
-
 }
+
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
@@ -28,8 +28,8 @@ public:
         vector<vector<int>>ans;
         do{
             ans.push_back(nums);
-        }
-        while(nextpermutations(nums));
-    
-    return ans;
-}};
+        }while(nextp(nums));
+        return ans;
+        
+    }
+};

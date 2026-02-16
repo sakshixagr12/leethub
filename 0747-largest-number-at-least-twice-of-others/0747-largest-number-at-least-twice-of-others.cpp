2ce50d4bb2d7e36f3943f9f981a09fef6482f615
Sakshi Agrahari <agraharisakku@gmail.com>
2026-02-16 20:51:33 +0530

    Time: 0 ms (100%), Space: 14 MB (14.71%) - LeetHub

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.cpp
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.cpp
@@ -12,15 +12,16 @@ public:
         //     if(nums[i]*2 > largest && largest!=nums[i]) return -1;
         // }
         // return ans;
-       int ans=0;
-       int largest=nums[0];
-       int slargest=nums[0];
-       for(int i=1;i<nums.size();i++){
-        if(nums[i]>largest && slargest!=nums[i]){
+       int ans=-1;
+       int largest=-1;
+       int slargest=-1;
+       for(int i=0;i<nums.size();i++){
+        if(nums[i]>largest){
             slargest=largest;
             largest=nums[i];
             ans=i;
         }
+        else if(nums[i]>slargest) slargest=nums[i];
        }
        if(slargest *2>largest) return -1;
        return ans;

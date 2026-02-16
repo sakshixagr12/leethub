b4934f24a81ca78a44e82b9032a9981beb8aeeb1
Sakshi Agrahari <agraharisakku@gmail.com>
2026-02-16 20:51:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.cpp
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     int dominantIndex(vector<int>& nums) {
-        int ans = 0;
-        int largest=nums[0];
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]>largest){
-                largest=nums[i];
-                ans=i;
-        }}
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]*2 > largest && largest!=nums[i]) return -1;
+        // int ans = 0;
+        // int largest=nums[0];
+        // for(int i=1;i<nums.size();i++){
+        //     if(nums[i]>largest){
+        //         largest=nums[i];
+        //         ans=i;
+        // }}
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]*2 > largest && largest!=nums[i]) return -1;
+        // }
+        // return ans;
+       int ans=0;
+       int largest=nums[0];
+       int slargest=nums[0];
+       for(int i=1;i<nums.size();i++){
+        if(nums[i]>largest && slargest!=nums[i]){
+            slargest=largest;
+            largest=nums[i];
+            ans=i;
         }
-        return ans;
-        
+       }
+       if(slargest *2>largest) return -1;
+       return ans;
     }
 };

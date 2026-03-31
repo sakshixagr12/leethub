19f3de201475370a82ff9553618937f9f06c604a
Sakshi Agrahari <agraharisakku@gmail.com>
2026-04-01 00:41:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int>mpp;
-        for(auto it : nums) mpp[it]++;
-        for(auto it : mpp){
-            if(it.second > nums.size()/2) return it.first;
+        // unordered_map<int,int>mpp;
+        // for(auto it : nums) mpp[it]++;
+        // for(auto it : mpp){
+        //     if(it.second > nums.size()/2) return it.first;
+        // }
+        // return -1;
+        int cnt = 0;
+        int el=nums[0];
+        for(auto it : nums){
+            if(cnt==0){
+                cnt++;
+                el=it;
+            } else if (el==it) cnt++;
+             else cnt--;
         }
+        int cnt1=0;
+        for(auto it : nums){
+            if(it==el)cnt1++;
+        }
+        if(cnt1> nums.size()/2) return el;
         return -1;
     }
 };

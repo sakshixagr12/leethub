113243df373421378dfccbf9608bce0abff57777
Sakshi Agrahari <agraharisakku@gmail.com>
2026-04-09 23:15:21 +0530

    Time: 0 ms (100%), Space: 8.9 MB (32.98%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
+        
        int j = s.size()-1;
-       int ans=0;
-       while(j>=0 &&s[j]==' ')j--;
+       int ans = 0;
+       while (j>=0 && s[j]==' ')j--;
        
-       while(j>=0 &&s[j]!=' '){
+       while ( j>=0 && s[j]!=' ') {
         ans++;
         j--;
-
        }
+
         return ans;
     }
 };

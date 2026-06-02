30a36c1e3226a812dcc3811838c5caeebed5449e
Sakshi Agrahari <agraharisakku@gmail.com>
2026-06-02 21:04:27 +0530

    Time: 55 ms (32.2%), Space: 92.8 MB (16.18%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int cnt=0;
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1]){
-            cnt++;
-            break;
-            }
-        }
-        if(cnt>0)
-        return true;
-        else return false;
-
-        
+       unordered_map<int,int>mpp;
+       for(auto it:nums)mpp[it]++;
+       for(auto it:mpp){
+        if(it.second!=1) return true;
+       }
+       return false; 
     }
 };

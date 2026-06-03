31107a43e0fd796b4044643216c7d9fc01af2b42
Sakshi Agrahari <agraharisakku@gmail.com>
2026-06-03 12:27:29 +0530

    Time: 3 ms (20.35%), Space: 36.4 MB (19.27%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
+        // int n = nums.size();
+        // for(int i = 0 ; i<n ; i++){
+        //     long long lsum=0;
+        //     long long rsum=0;
+        //     //leftSum
+        //     for(int j = 0 ; j<i ; j++){
+        //         lsum+=nums[j];
+        //     }
+        //     //rightSum
+        //     for(int j = i+1 ; j<n ; j++){
+        //         rsum+=nums[j];
+        //     }
+        //     if(lsum==rsum)return i;
+        // }
+        // return -1;
+
         int n = nums.size();
-        for(int i = 0 ; i<n ; i++){
-            long long lsum=0;
-            long long rsum=0;
-            //leftSum
-            for(int j = 0 ; j<i ; j++){
-                lsum+=nums[j];
-            }
-            //rightSum
-            for(int j = i+1 ; j<n ; j++){
-                rsum+=nums[j];
-            }
-            if(lsum==rsum)return i;
+        vector<int>prefix(n);
+        prefix[0]=nums[0];
+        int lsum=0,rsum=0;
+        for(int i =1;i<n ;i++)prefix[i]=prefix[i-1]+nums[i];
+        int total = prefix[n-1];
+        for(int i = 0 ;i<n;i++){
+            if(i==0) lsum=0;
+            else lsum=prefix[i-1];
+            rsum=total-prefix[i];
+            if(lsum==rsum) return i;
         }
         return -1;
     }

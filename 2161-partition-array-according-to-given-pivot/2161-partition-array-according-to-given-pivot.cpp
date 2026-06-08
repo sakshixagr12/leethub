5e0e0ca008543768cfafee5bdcdfbcc280ad30e0
Sakshi Agrahari <sakshi.agrahari.ind@gmail.com>
2026-06-08 13:07:22 +0530

    Time: 15 ms (11.39%), Space: 133.3 MB (62.93%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -2,19 +2,35 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         
-        vector<int> less , eq , grt;
+        // vector<int> less , eq , grt;
 
-        for ( auto it : nums ) {
-            if ( it > pivot) grt.push_back(it);
-            else if ( it == pivot ) eq.push_back(it);
-            else less.push_back(it);
-        }
+        // for ( auto it : nums ) {
+        //     if ( it > pivot) grt.push_back(it);
+        //     else if ( it == pivot ) eq.push_back(it);
+        //     else less.push_back(it);
+        // }
+
+        // vector<int> answer;
+
+        // for ( auto el : less ) answer.push_back(el);
+        // for ( auto el : eq )   answer.push_back(el);
+        // for ( auto el : grt )  answer.push_back(el);
 
         vector<int> answer;
-        
-        for ( auto el : less ) answer.push_back(el);
-        for ( auto el : eq )   answer.push_back(el);
-        for ( auto el : grt )  answer.push_back(el);
+
+        for ( auto it : nums ) {
+            if ( it < pivot )
+                answer.push_back(it);
+        }
+        for ( auto it : nums ) {
+            if ( it == pivot )
+                answer.push_back(it);
+        }
+        for ( auto it : nums) {
+            if ( it > pivot ) {
+                answer.push_back(it);
+            }
+        }
 
         return answer;
     }

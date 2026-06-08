79606df8b0b401b16e817a1679a236d84bb8bc75
Sakshi Agrahari <sakshi.agrahari.ind@gmail.com>
2026-06-08 13:41:11 +0530

    Time: 22 ms (59.81%), Space: 17.5 MB (52.67%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>>result;
+
+        vector<vector<int>>res; 
         sort(nums.begin(),nums.end());
-        if (nums.size()<4){
+        int n = nums.size();
+        if (n<4){
             return {};
-
         }
-        if (nums[0] == nums.back()) {
-            if (4LL * nums[0] == target) 
-                return {{nums[0], nums[0], nums[0], nums[0]}};
-            else return {};
-        }
-
-        for(int i=0;i< nums.size()-3;i++){
-           if(i>0 && nums[i]==nums[i-1])continue;
-           if ((long long)nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target) break;
-
-            
-            for(int j=i+1;j<nums.size()-2;j++){
-                if(j>i+1 && nums[j]==nums[j-1])continue;
-                int k =j+1;
-                int l=nums.size()-1;
-                while(k<l){
-                    long long sum=(long long)nums[i]+nums[j]+nums[k]+nums[l];
-                    if(sum>target)l--;
-                    else if(sum<target)k++;
-                    else{
-                        result.push_back({nums[i],nums[j],nums[k],nums[l]});
-                      
-                        while (k < l && nums[k] == nums[k + 1]) k++;
-                        while (k < l && nums[l] == nums[l - 1]) l--;
+        // if(n==1){
+        //     if(nums[0]==target) res.push_back({nums[0]});
+        // }
+        for(int i = 0 ; i < nums.size()-3 ; i++ ) {
+            if(i > 0 && nums[i]==nums[i-1]) continue;
+            int j = i+1;
+            while (j < n-2) {
+            //if (j>i && nums[j]==nums[j-1]) continue;
+                int k = j+1;
+                int l = n-1;
+                while ( k<l ) {
+                    long long sum =  (long long)nums[i]+nums[j]+nums[k]+nums[l];
+                    if ( sum == target) {
+                        res.push_back({nums[i],nums[j],nums[k],nums[l]});
                         k++;
                         l--;
-
+                        while ( k<l && nums[k] == nums[k-1]) k++;
+                        while ( l>k && nums[l] == nums[l+1]) l--;
                     }
+                    else if ( sum > target) l--;
+                    else k++;
                 }
+                j++;
+                while ( j < n-2 && nums[j]==nums[j-1]) j++;
             }
-        }
-        
-
-        return result;   
+        }  
+        return res;
     }
 };

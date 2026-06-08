e8569ffe5bc0ced7a2432c086a01b2338c52779a
Sakshi Agrahari <sakshi.agrahari.ind@gmail.com>
2026-06-08 22:48:21 +0530

    Time: 0 ms (100%), Space: 10 MB (10.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // unordered_map<char,int>mpp;
-        // for(auto it : s)mpp[it]++;
         if(s.size()!=t.size()) return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        int i = 0, j=0;
-        while(i<s.size()){
-            if(s[i]!=t[j]) return false;
-            i++;
-            j++;
+        unordered_map<char,int>mpp;
+        for(auto it:s) mpp[it]++;
+
+        for(auto it :t){
+            mpp[it]--;
+        }
+        for(auto it:mpp){
+            if(it.second!=0) return false;
         }
         return true;
     }

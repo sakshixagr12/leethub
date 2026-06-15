1516abbf1c7c5beadca1f5e0c874eececb485f11
Sakshi Agrahari <sakshi.agrahari.ind@gmail.com>
2026-06-15 22:46:19 +0530

    Time: 5 ms (16.77%), Space: 312.1 MB (54.78%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -12,24 +12,35 @@ class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
         if(head==nullptr|| head->next==nullptr) return nullptr;
-        ListNode *temp=head;
-        int n = 0;
-        while ( temp != nullptr){
-            n++;
-            temp = temp -> next ;
+        // ListNode *temp=head;
+        // int n = 0;
+        // while ( temp != nullptr){
+        //     n++;
+        //     temp = temp -> next ;
+        // }
+        // int res= n/2;
+        // temp=head;
+        // while( temp != nullptr){
+        //     res--;
+        //     if(res==0){
+        //         ListNode *middle = temp->next;
+        //         temp->next = temp->next->next;
+        //         delete middle;
+        //         break;
+        //     }
+        //     temp=temp->next;
+        // }
+        // return head;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        fast=fast->next->next;
+        while(fast!=nullptr&&fast->next != nullptr){
+            slow= slow->next;
+            fast=fast->next->next;
         }
-        int res= n/2;
-        temp=head;
-        while( temp != nullptr){
-            res--;
-            if(res==0){
-                ListNode *middle = temp->next;
-                temp->next = temp->next->next;
-                delete middle;
-                break;
-            }
-            temp=temp->next;
-        }
+        ListNode *middle= slow->next;
+        slow->next=slow->next->next;
+        delete middle;
         return head;
     }
 };

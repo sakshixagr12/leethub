db0137d7eac77e8452fb4c47767a3ad69ea7d58c
Sakshi Agrahari <sakshi.agrahari.ind@gmail.com>
2026-07-04 11:36:07 +0530

    Time: 8 ms (99.16%), Space: 139.2 MB (42.66%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -4,15 +4,19 @@ public:
         int n = nums.size();
         vector<int> ans;
         deque<int> dq;
-        for (int i = 0; i <n; i++) {
-            while (!dq.empty() && dq.front() <= i - k)
-                dq.pop_front();
-            while (!dq.empty() && nums[dq.back()] < nums[i])
-                dq.pop_back();
+        //first window
+        for(int i =0;i<k;i++){
+            while(!dq.empty()&&nums[dq.back()]<=nums[i]) dq.pop_back();
             dq.push_back(i);
-            if (i >= k - 1)
-                ans.push_back(nums[dq.front()]);
         }
+        for(int i =k;i<n;i++) {
+            ans.push_back(nums[dq.front()]);
+            while(!dq.empty()&&dq.front()<i-k+1)dq.pop_front();
+            while(!dq.empty()&&nums[dq.back()]<=nums[i]) dq.pop_back();
+            dq.push_back(i);
+        }
+        ans.push_back(nums[dq.front()]);
+        
 
         return ans;
     }

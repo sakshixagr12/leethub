e01408fab04b3f145fd104af0faeaaa89b0bd6f6
Sakshi Agrahari <sakshi.agrahari.ind@gmail.com>
2026-07-20 21:57:13 +0530

    Time: 1 ms (50.56%), Space: 14 MB (84.98%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
 
-    void findComb(int idx,int target,vector<int>&arr,vector<int>&ds,vector<vector<int>>&ans){
-        
-            if(target==0){
-                ans.push_back(ds);
-                return;
-            }
-        
-        for(int i = idx;i<arr.size();i++) {
-            if(i>idx && arr[i]== arr[i-1]) 
-                continue;
-            if (arr[i]>target) break;
+    void func(int idx,int target,vector<int>&arr,vector<vector<int>>&ans,vector<int>&ds){
+        if(target==0){
+            ans.push_back(ds);
+            return;
+        }
+        for(int i = idx;i<arr.size();i++){
+            //skip duplicates
+            if(i>idx&&arr[i]==arr[i-1]) continue;
+            if(arr[i]>target) break;
+            
             ds.push_back(arr[i]);
-            findComb(i+1,target-arr[i],arr,ds,ans);
+            func(i+1,target-arr[i],arr,ans,ds);
             ds.pop_back();
-        }  
+        }
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
         vector<int>ds;
         vector<vector<int>>ans;
-        sort(candidates.begin(),candidates.end()); 
-        
-        findComb(0,target,candidates,ds,ans);
-    
+        func(0,target,candidates,ans,ds);
         return ans;
     }
 };

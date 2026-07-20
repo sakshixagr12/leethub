21cb9a88eb4e85d01d1a1d289aed6865ff90891e
Sakshi Agrahari <sakshi.agrahari.ind@gmail.com>
2026-07-20 22:12:18 +0530

    Time: 0 ms (100%), Space: 8.6 MB (82.97%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    void func(int i,int k,int n ,vector<vector<int>>&ans,vector<int>&ds){
+        if(k==0&&n==0){
+            ans.push_back(ds);
+            return;
+        }
+        for(int cnt=i;cnt <= 9;cnt++){
+            if(cnt>n)break;
+            ds.push_back(cnt);
+            func(cnt+1,k-1,n-cnt,ans,ds);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+       vector<vector<int>>ans;
+       vector<int>ds;
+       func(1,k,n,ans,ds);
+       return ans; 
+    }
+};

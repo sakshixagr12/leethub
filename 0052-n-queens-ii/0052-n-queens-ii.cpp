139613ecab85e4da0d7b6ae3234e7305fd1316a8
Sakshi Agrahari <sakshi.agrahari.ind@gmail.com>
2026-07-26 23:01:12 +0530

    Time: 1 ms (59.73%), Space: 8.3 MB (55.02%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool isSafe(int row,int col,vector<string>&board,int n){
+        int r=row;
+        int c=col;
+        //upper left diagonal
+        while(r>=0&&c>=0){
+            if(board[r][c]=='Q')return false;
+            r--;
+            c--;
+        }
+        //left col
+        c=col;
+        r=row;
+        while(c>=0){
+            if(board[r][c]=='Q')return false;
+            c--;
+        }
+        //lower left diagonal
+        c=col;
+        r=row;
+        while(r<n&&c>=0){
+            if(board[r][c]=='Q')return false;
+            r++;
+            c--;
+        }
+        return true;
+    }
+    void solve(int col,int n ,vector<string>&board,int &ans){
+        if(col==n){
+            ans++;
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(isSafe(row,col,board,n)){
+                board[row][col]='Q';
+                solve(col+1,n,board,ans);
+                board[row][col]='.';
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        int answer=0;
+        vector<string>board(n);
+        string s(n,'.');
+        for(int i =0;i<n;i++) board[i]=s;
+        solve(0,n,board,answer);
+        return answer;
+    }
+};

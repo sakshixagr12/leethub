3b28c6934ceefad9234e769bfb698d433dc28041
Sakshi Agrahari <sakshi.agrahari.ind@gmail.com>
2026-08-13 12:29:29 +0530

    Time: 3 ms (40.91%), Space: 9.3 MB (83.35%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> m1;
+        unordered_map<char, char> m2;
+
+        for (int i = 0; i < s.size(); i++) {
+            char c1 = s[i];
+            char c2 = t[i];
+
+            if (m1.find(c1) != m1.end() && m1[c1] != c2)
+                return false;
+
+            if (m2.find(c2) != m2.end() && m2[c2] != c1)
+                return false;
+
+            m1[c1] = c2;
+            m2[c2] = c1;
+        }
+
+        return true;
+    }
+};

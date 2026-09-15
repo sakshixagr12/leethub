3fcbe34404bd9de509e8e678eb752d2a0570ff8b
Sakshi Agrahari <sakshi.agrahari.ind@gmail.com>
2026-09-15 12:21:17 +0530

    Time: 39 ms (36.51%), Space: 63.3 MB (5.77%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -26,9 +26,42 @@ public:
         dp[n][s] = include+exclude;
         return dp[n][s];
     }
+    int solve_tab(vector<int>& coins, int& sum) {
+
+        int n = coins.size();
+        vector<vector<unsigned int>>dp(n+1,vector<unsigned int>(sum+1,0));
+
+        for(int i=0; i<=n; i++)
+        {
+            dp[i][0] = 1; // if sum is zero, then answer is already 1
+        }
+
+        for(int i = 1; i<=n; i++)
+        {
+            for(int j=1; j<=sum; j++) {
+
+                // if current coin is greater than the required sum
+                if (coins[i-1] > j) 
+                {
+                    //need to exclude this
+                    dp[i][j] = dp[i-1][j];
+                }
+                else{
+                // including/rep.
+                unsigned int include = dp[i][j-coins[i-1]];
+                    //exclude
+                unsigned int exclude = dp[i-1][j];
+                    // possible ways till dp[i][j]
+                dp[i][j] = include+exclude;
+                }
+            }
+        }
+        return dp[n][sum];
+    }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<vector<int>> dp( n + 1, vector<int>(amount + 1, -1));
-        return solve(amount, coins, dp, n);
+       // return solve(amount, coins, dp, n);
+       return (int)solve_tab(coins,amount);
     }
 };
